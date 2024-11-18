bbd6325a134754b2033d812b81ee857c0d8c0fee
Your name <Your email>
2024-11-18 11:52:58 +0100

    suppression index

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./models/connection');
 
-
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 var advicesRouter = require('./routes/advices');
@@ -25,7 +23,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/advices', advicesRouter);
